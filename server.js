161923640e54c5f248101c172e96d6d510c09635
ANKIT SHARMA <100486645+ANKIT-777@users.noreply.github.com>
2023-07-11 19:33:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,23 +8,20 @@ const userRoutes = require('./routes/route');
 app.use(express.json());
 
 app.use('/users', userRoutes);
-// server.js
+
 
 require('dotenv').config();
 
 
-// Handle invalid routes
 app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
 
-// Global error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Internal server error' });
 });
 
-// Start the server
 const port = 3030;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
